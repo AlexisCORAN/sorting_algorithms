ee40b01eab635a8edc2b1dda20d6e4563509c135
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-13 20:28:15 +0000

    Revert "Add again function swap"
    This reverts commit e1a3176fd52ec1dce123a2842535ccb1481809dd.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+* swap - function that swaps the two integers.
+*
+*@a: first integer number.
+*
+*@b: second integer number.
+*/
+void swap(int *a, int *b)
+{
+	unsigned int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
 * bubble_sort - sorts an array of integers in ascending order
 *		using the Bubble sort algorithm.
 *
@@ -11,7 +27,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j, temp, swapped = 0;
+	unsigned int i, j, swapped = 0;
 	size_t n = size;
 
 	for (i = 0; i < n - 1; i++)
@@ -20,9 +36,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap(&array[j], &array[j + 1]);
 				print_array(array, n);
 				swapped = 1;
 			}
