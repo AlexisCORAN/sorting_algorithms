847bc9e53036f8bdc752b128c75177494151ca89
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-16 13:13:53 +0000

    Add the Shell sort algorithm using the Knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+* shell_sort - sorts an array of integers in ascending order using
+*		the Shell sort algorithm, using the Knuth sequence
+*
+*@array: array of integers.
+*
+*@size: size of array.
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	unsigned int interval = 1, i, j;
+	int idx;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (interval = 1; interval <= size / 3;)
+	{
+		interval = (interval * 3) + 1;
+	}
+
+	while (interval > 0)
+	{
+		for (i = interval; i < size; i++)
+		{
+			temp = array[j];
+			j = i;
+			while (j > interval - 1 && array[j - interval] >= temp)
+			{
+				array[j] = array[j - interval];
+				j -= interval;
+			}
+			array[j] = temp;
+		}
+		interval = (interval - 1) / 3;
+		idx++;
+		print_array(array, size);
+	}
+}
